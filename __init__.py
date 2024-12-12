4dcaaa9ef305602939b5f5130eafffa95d4e82b9
INSTITUT UNIVERSITAIRE DE LA COTE <iuc@iuc>
2024-12-12 11:19:46 +0100

    Ajout dans init.py

--- __init__.py
+++ __init__.py
@@ -13,6 +13,7 @@ def register():
         health_services.HealthService,
         health.Insurance,
         health.Invoice,
+        health.InvoiceLine
         module='z_health_extra', type_='model')
     Pool.register(
         module='z_health_extra', type_='wizard')
