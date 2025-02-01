15d3f323551865ea5ae4edab5901a901c68e48e2
ManuelZe <lilianmanuelze@yahoo.com>
2025-02-01 13:47:18 +0100

    Facture 0 FCFA compris

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 
 from trytond.pool import Pool
 from .import health_services
-from . import health, move
+from . import health
 from .wizard import wizard_health_insurance
 
 __all__ = ['register']
@@ -21,7 +21,6 @@ def register():
         health.Commission,
         health.GnuHealthPatientLabTest,
         health.ImagingTestRequest,
-        move.Move,
         module='z_health_extra', type_='model')
     Pool.register(
         health.PayInvoice,
