67531eef637d028e6e01550adc9f72bcb832873c
ManuelZe <lilianmanuelze@yahoo.com>
2025-02-04 14:28:13 +0100

    TOUT TOUT TOUT TOUT TOUT TOUT TOUT OK OK OK OK OK OK OK OK OK OK

--- health.py
+++ health.py
@@ -110,14 +110,9 @@ class Lab(metaclass=PoolMeta):
         
         liste_paillasse = []
         for record in records :
-            print("Cas record.test.test_type.name", record.test.test_type.name)
             if record.test.test_type.name :
-                print("Cas record.test.test_type.name", record.test.test_type.name)
                 if record.test.test_type.name not in liste_paillasse:
-                    print("Cas 2222222222222222222")
                     liste_paillasse.append(record.test.test_type.name)
-        
-        print("gerrrrrrrrrrrrrrrrg", liste_paillasse)
         
         return liste_paillasse
 
