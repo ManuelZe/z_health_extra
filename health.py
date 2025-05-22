768276a3bd056ab7deaa748e6f300fcec809fc76
ManuelZe <lilianmanuelze@yahoo.com>
2025-05-22 13:57:52 +0200

    Bref 10

--- health.py
+++ health.py
@@ -594,8 +594,7 @@ class Invoice(metaclass=PoolMeta):
         liste_docteurs = {}
         for record in records:
             print("------",record.party.clef)
-            if record.party.clef != None:
-                
+            if record.party.clef != None or record.party.clef != "":
                 docteur = record.party.name+" "+record.party.lastname
                 list_element = []
                 for line in record.lines:
@@ -617,7 +616,7 @@ class Invoice(metaclass=PoolMeta):
         # {"JUDITH": (montant, impot, net_a_payer), "FRED": (montant, impot, net_a_payer), "MARINA": (montant, impot, net_a_payer)}
         liste_docteurs = {}
         for record in records:
-            if record.party.clef == None :
+            if record.party.clef == None or record.party.clef != "":
                 docteur = record.party.name+" "+record.party.lastname
                 list_element = []
                 for line in record.lines:
