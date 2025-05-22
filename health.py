353a7c6642bccbece42367e4b9fc5e9d19dc3d30
ManuelZe <lilianmanuelze@yahoo.com>
2025-05-22 13:23:25 +0200

    Bref 10

--- health.py
+++ health.py
@@ -593,7 +593,7 @@ class Invoice(metaclass=PoolMeta):
 
         liste_docteurs = {}
         for record in records:
-            if record.party.numero_carte != None:
+            if record.party.numero_carte != "":
                 docteur = record.party.name+" "+record.party.lastname
                 list_element = []
                 for line in record.lines:
@@ -615,7 +615,7 @@ class Invoice(metaclass=PoolMeta):
         # {"JUDITH": (montant, impot, net_a_payer), "FRED": (montant, impot, net_a_payer), "MARINA": (montant, impot, net_a_payer)}
         liste_docteurs = {}
         for record in records:
-            if record.party.numero_carte == None :
+            if record.party.numero_carte == "" :
                 docteur = record.party.name+" "+record.party.lastname
                 list_element = []
                 for line in record.lines:
