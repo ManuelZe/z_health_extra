eecaea44e8068e1054fe8408bdf12e68418fa450
ManuelZe <lilianmanuelze@yahoo.com>
2025-05-22 12:08:11 +0200

    Bref 9

--- health.py
+++ health.py
@@ -593,20 +593,19 @@ class Invoice(metaclass=PoolMeta):
 
         liste_docteurs = {}
         for record in records:
-            if record.party.clef == None:
-                continue
-            docteur = record.party.name+" "+record.party.lastname
-            list_element = []
-            for line in record.lines:
-                if docteur in liste_docteurs.keys():
-                    liste_docteurs[docteur][0] = liste_docteurs[docteur][0] + record.total_amount
-                    liste_docteurs[docteur][1] = record.party.numero_carte
-                    liste_docteurs[docteur][2] = record.party.clef
-                else:
-                    list_element.append(record.total_amount)
-                    list_element.append(record.party.numero_carte)
-                    list_element.append(record.party.clef)
-                    liste_docteurs[docteur] = list_element
+            if record.party.clef != None:
+                docteur = record.party.name+" "+record.party.lastname
+                list_element = []
+                for line in record.lines:
+                    if docteur in liste_docteurs.keys():
+                        liste_docteurs[docteur][0] = liste_docteurs[docteur][0] + record.total_amount
+                        liste_docteurs[docteur][1] = record.party.numero_carte
+                        liste_docteurs[docteur][2] = record.party.clef
+                    else:
+                        list_element.append(record.total_amount)
+                        list_element.append(record.party.numero_carte)
+                        list_element.append(record.party.clef)
+                        liste_docteurs[docteur] = list_element
 
         return liste_docteurs
         
