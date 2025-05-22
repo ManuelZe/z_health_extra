15e5145cb2d8f24c914722b9b169abf3f257744c
ManuelZe <lilianmanuelze@yahoo.com>
2025-05-22 14:14:31 +0200

    Bref 10

--- health.py
+++ health.py
@@ -593,19 +593,20 @@ class Invoice(metaclass=PoolMeta):
 
         liste_docteurs = {}
         for record in records:
-            if record.party.clef != None or record.party.clef != "":
-                docteur = record.party.name+" "+record.party.lastname
-                list_element = []
-                for line in record.lines:
-                    if docteur in liste_docteurs.keys():
-                        liste_docteurs[docteur][0] = liste_docteurs[docteur][0] + record.total_amount
-                        liste_docteurs[docteur][1] = record.party.numero_carte
-                        liste_docteurs[docteur][2] = record.party.clef
-                    else:
-                        list_element.append(record.total_amount)
-                        list_element.append(record.party.numero_carte)
-                        list_element.append(record.party.clef)
-                        liste_docteurs[docteur] = list_element
+            if record.party.clef:
+                if len(record.party.clef) != 0 :
+                    docteur = record.party.name+" "+record.party.lastname
+                    list_element = []
+                    for line in record.lines:
+                        if docteur in liste_docteurs.keys():
+                            liste_docteurs[docteur][0] = liste_docteurs[docteur][0] + record.total_amount
+                            liste_docteurs[docteur][1] = record.party.numero_carte
+                            liste_docteurs[docteur][2] = record.party.clef
+                        else:
+                            list_element.append(record.total_amount)
+                            list_element.append(record.party.numero_carte)
+                            list_element.append(record.party.clef)
+                            liste_docteurs[docteur] = list_element
 
         return liste_docteurs
         
@@ -615,7 +616,7 @@ class Invoice(metaclass=PoolMeta):
         # {"JUDITH": (montant, impot, net_a_payer), "FRED": (montant, impot, net_a_payer), "MARINA": (montant, impot, net_a_payer)}
         liste_docteurs = {}
         for record in records:
-            if record.party.clef == None or record.party.clef != "":
+            if not record.party.clef == None or record.party.clef == "":
                 docteur = record.party.name+" "+record.party.lastname
                 list_element = []
                 for line in record.lines:
